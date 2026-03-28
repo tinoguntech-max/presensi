424af3f48e4442631e9e4d4a377d47e5c313354c
tinogun bambang <tinoguntech@gmail.com>
2026-03-28 13:19:38 +0700

    deploy: update config production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['presensi.tinodev.site', 'api.tinodev.site'],
+    allowedHosts: ['presensi.tamanet.site', 'api.tamanet.site', 'presensi.tinodev.site', 'api.tinodev.site'],
   },
 });
